da72348e6c58591f496549e71d73291e72c58cf0
youngunyou <ygyou@me.com>
2019-10-06 00:41:45 +0800

    채팅앱구현

--- app.js
+++ app.js
@@ -4,26 +4,25 @@ const fs = require('fs');
 const socketio = require('socket.io');
 const express = require('express');
 
-const app = express();
-// 서버용 인증서 옵션 ///
+//// 서버용 인증서 ///
 const options = {
   key: fs.readFileSync('/etc/letsencrypt/live/www.unitedin.kr/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/www.unitedin.kr/cert.pem')
 };
 
-///////// 로컬 인증서 옵션 //////////
+/////// 테스트용 로컬 인증서 //////////
 // const options = {
-//   key: fs.readFileSync(__dirname + '/openSSLcert/key.pem'),
-//   cert: fs.readFileSync(__dirname + '/openSSLcert/key-cert.pem')
+//   key: fs.readFileSync(__dirname + '/openSSLcert/file.pem'),
+//   cert: fs.readFileSync(__dirname + '/openSSLcert/file.crt')
 // };
 
 // 웹서버와 소켓을 생성합니다.
-                      // 웹서버 생성
+const app = express();                        // 웹서버 생성
 const io = socketio();                        // 웹소켓 생성
 const server = http.createServer(app);
 const serverSSL = https.createServer(options,app);
 
-io.attach(serverSSL);                      // 웹서버와 소켓서버 연결
+io.attach(serverSSL);                      // 웹서버와 웹소켓 연결
 
 // 라우팅
 app.get('/', (req,res)=>{
@@ -33,17 +32,15 @@ app.get('/', (req,res)=>{
   });
 });
 
-let clientId = 0;
-let roomName = null;
+// let clientId = 0;
+// let roomName = null;
 io.sockets.on('connection',(socket)=>{
-
-  clientId = socket.id;
-
-  socket.on('join',(data)=>{         // 클라이언트가 룸생성해서 이벤트 발송
-    roomName = data;
-    socket.join(roomName);           // 클라이언트를 방에 집어넣는 메소드
-    //console.log('join roomName: ' + roomName);
-  });
+  // clientId = socket.id;
+  // socket.on('join',(data)=>{         // 클라이언트가 룸생성해서 이벤트 발송
+  //   roomName = data;
+  //   socket.join(roomName);           // 클라이언트를 방에 집어넣는 메소드
+  //   //console.log('join roomName: ' + roomName);
+  // });
 
 ////////////////////////////////////////////////////////////////
 // clengMsg이벤트(커스텀이벤트)를 통해 클라이언트로부터 메시지를 받으면
@@ -54,15 +51,15 @@ io.sockets.on('connection',(socket)=>{
 ////////////////////////////////////////////////////////////////
   socket.on('clientMsg',(data)=>{
     // 클라이언트로부터 받은 메세지를 해당 룸에 있는 클라이언트들에게 전송
-    io.sockets.in(roomName).emit('serverMsg', data); //room 지정하여 전송
-    // io.sockets.emit('serverMsg',data);              //Public 전송
+    // io.sockets.in(roomName).emit('serverMsg', data); //room 지정하여 전송
+     io.sockets.emit('serverMsg',data);              //Public 전송
     // io.sockets.to(clientId).emit('serverMsg',data); //private 전송
     // socket.broadcast.emit('serverMsg',data);        //broadcast 전송
   });
 });
  
 // 서버실행
-server.listen(80, function () {
+server.listen(3000, function () {
     console.log('Server is running at http://localhost:3000');
 });
 serverSSL.listen(443, function () {
